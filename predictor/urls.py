330b048c847d04b23dfbb82259d6ad3a333cb495
Goal651 <bugiriwilson651@gmail.com>
2026-03-10 08:52:31 +0200

    added different pages for posting

--- predictor/urls.py
+++ predictor/urls.py
@@ -3,4 +3,8 @@ from predictor import views
 
 urlpatterns = [
     path("", views.data_exploration_view, name="home"),
+    path("data-exploration/", views.data_exploration_view, name="data_exploration"),
+    path("regression/", views.regression_analysis_view, name="regression_analysis"),
+    path("classification/", views.classification_analysis_view, name="classification_analysis"),
+    path("clustering/", views.clustering_analysis_view, name="clustering_analysis"),
 ]
